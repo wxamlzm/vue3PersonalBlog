021d2d647523c8fe6219b68d0fbc53fb9863cdd5
wxamlzm <zd5024@163.com>
2024-09-25 13:48:23 +0800

    UPDATE：分包

--- vite.config.ts
+++ vite.config.ts
@@ -9,13 +9,24 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // 使用defineConfig来配置vite
 // 这个函数接收一个配置对象或一个返回配置对象的函数
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  console.log(mode)
+
   // loadEnv函数用于加载指定模式下的环境变量
   // 参数： 模式（如‘development'），环境文件所在目录，环境变量前缀（空字符串表示加载所有变量）
   const env = loadEnv(mode, process.cwd(), '')
-  console.log(`env:${env.VITE_API_SERVER_URL}`)
 
   return {
     plugins: [vue(), vueDevTools()],
+
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['vue', 'axios', 'pinia', 'vue-router']
+          }
+        }
+      }
+    },
 
     server: {
       proxy: {
